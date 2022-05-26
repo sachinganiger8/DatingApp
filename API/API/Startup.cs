d97e0c12c903fd512bf98d4ccfbf4c8a17d9bf8a
Sachin Ganiger <Sachin.Ganiger@zettalabs.com>
2022-05-26 15:08:10 +0530

    End of Section 3

--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -38,6 +38,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
