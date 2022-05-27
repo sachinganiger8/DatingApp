22965a991430c100e687b4bd957d4cdcffef53bb
Sachin Ganiger <Sachin.Ganiger@zettalabs.com>
2022-05-27 18:49:12 +0530

    End of section 4

---
+++ API/API/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        public DataContext _dataContext { get; set; }
+        public ITokenService _tokenService { get; set; }
+        public AccountController(DataContext dataContext,ITokenService tokenService)
+        {
+            _dataContext = dataContext;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
+        {
+            if (await UserExists(registerDTO.Username))
+                return BadRequest("Username already taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _dataContext.Add(user);
+            await _dataContext.SaveChangesAsync();
+
+            return new UserDTO
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _dataContext.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
+        {
+            var user = await _dataContext.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username.ToLower());
+
+            if (user == null)
+                return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            if (user.PasswordHash.Length != computedHash.Length)
+                return Unauthorized("Invalid Username");
+            else
+            {
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != user.PasswordHash[i])
+                        return Unauthorized("Invalid Password");
+                }
+            }
+
+            return new UserDTO
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+    }
+}
--- API/API/Controllers/UsersController.cs
+++ API/API/Controllers/UsersController.cs
@@ -1,14 +1,13 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         public DataContext _dataContext { get; set; }
         public UsersController(DataContext dataContext)
@@ -17,12 +16,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _dataContext.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _dataContext.Users.FindAsync(id);
