703347bfbff0e5a3cad68909bd40f50eaee3898a
Sachin Ganiger <Sachin.Ganiger@zettalabs.com>
2022-06-02 01:35:26 +0530

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +13,7 @@ export class NavComponent implements OnInit {
     username:'sachin',
     password:'Password'
   };
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService,private router:Router,private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -20,12 +22,15 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response=>{
       console.log(response);
+      this.router.navigateByUrl('/members');
     },error=>{
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
