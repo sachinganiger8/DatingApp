87a3ddd20da052643c01367cd04eac906b28e8dc
Sachin Ganiger <Sachin.Ganiger@zettalabs.com>
2022-06-09 00:51:06 +0530

    End of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  baseUrl=environment.apiUrl;
+  members:Member[];
 
-  constructor() { }
+  constructor(private memberService:MemberService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers(){
+     this.memberService.getMembers().subscribe(members=>{
+       this.members=members;
+     });
+  }
 }
